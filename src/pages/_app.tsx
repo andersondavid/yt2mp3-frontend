041d1728b1b137eadd3ec6f2c927005e323031a8
Anderson David <andersondavidfreitas@gmail.com>
2023-04-14 19:56:51 -0300

    remove context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { DataContext } from "../context/DataContext";
-import { StartServiceFunctionContext } from "../context/StartServiceFunctionContext";
-import { ConvertingContext } from "../context/ConvertingContext";
-
 export default function App({ Component, pageProps }: AppProps) {
-	const contextData = {
-		url: "",
-		quality: "",
-	};
-
-	const startService = {
-		cb: () => {},
-	};
-
-	const convertingData = {
-		videoName: "",
-		loading: false,
-	};
-
-	return (
-		<DataContext.Provider value={contextData}>
-			<StartServiceFunctionContext.Provider value={startService}>
-				<ConvertingContext.Provider value={convertingData}>
-					<Component {...pageProps} />
-				</ConvertingContext.Provider>
-			</StartServiceFunctionContext.Provider>
-		</DataContext.Provider>
-	);
+	return <Component {...pageProps} />;
 }
