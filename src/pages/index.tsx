997b5ae3a6eaa8d8429403fba7e5c25ac26b3e4f
Anderson David <andersondavidfreitas@gmail.com>
2023-04-13 22:28:30 -0300

    Delete CenterBox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,22 @@
-import CenterBox from "@/components/CenterBox";
-import { useState, useContext, Dispatch, SetStateAction } from "react";
-
-import { DataContext, DataContentContextType } from "../context/DataContext";
-import {
-	StartServiceFunctionContext,
-	StartServiceFunctionType,
-} from "../context/StartServiceFunctionContext";
-import { ConvertingContext, ConvertingContextType } from "../context/ConvertingContext";
-
-import { startService } from "../services/services";
-
-const startServiceResquest = (contextData: DataContentContextType, convertingData: ConvertingContextType) => {
-	startService({ contextData, convertingData });
-};
+import React from "react";
+import QualitySelect from "@/components/QualitySelect";
+import InputContainer from "@/components/InputContainer";
+import LogoContainer from "@/components/LogoContainer";
+import AboutContainer from "@/components/AboutContainer";
+import SubmitButton from "@/components/SubmitButton";
+import VideoTitle from "@/components/VideoTitle";
 
 export default function Home() {
-	const [loading, setLoading] = useState(false);
-	const contextData = useContext<DataContentContextType>(DataContext);
-	const startService = useContext<StartServiceFunctionType>(
-		StartServiceFunctionContext
-	);
-	const convertingData = useContext(ConvertingContext);
-
-	startService.cb = function () {
-		startServiceResquest(contextData, convertingData);
-	};
-
 	return (
-		<div className="h-screen w-screen flex">
-			{loading && <h2>CARREGANDO...</h2>}
-			<CenterBox />
+		<div className="h-screen w-screen flex justify-center p-20 md:p-0 md:items-center bg-white md:bg-stone-300 font-mono">
+			<div className="md:border-black md:border-2 md:border-b-4 p-4 rounded-lg bg-white">
+				<LogoContainer />
+				<VideoTitle videoTitle="Dragon ball super ULTRA INSTINCT THEME SONG 'Ka Ka Kachi Daze' EXTENDED" />
+				<InputContainer />
+				<SubmitButton />
+				<QualitySelect />
+				<AboutContainer />
+			</div>
 		</div>
 	);
 }
