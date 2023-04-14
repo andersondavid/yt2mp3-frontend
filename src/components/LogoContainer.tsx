17457157286eba90c60e123606a0d93a8f3d0c14
Anderson David <andersondavidfreitas@gmail.com>
2023-04-14 01:17:51 -0300

    add "priority" prop

--- src/components/LogoContainer.tsx
+++ src/components/LogoContainer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function LogoContainer() {
 	return (
 		<div className="text-center pb-4 relative h-16">
-			<Image src={'/logo.svg'} alt="logo image" fill />
+			<Image src={'/logo.svg'} alt="logo image" fill priority />
 		</div>
 	);
 }
